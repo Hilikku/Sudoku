bc0632d36a673373ff5595a7c5db94847b9bc492
somename <someemail>
2015-09-30 15:40:46 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -9,7 +9,7 @@ public class TestSudokuVerifier {
 	@Test
 	public void testWrongString() {
 		String candidateSolution = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
-			assertEquals(-1, SudokuVerifier.verify(candidateSolution));
+			assertEquals(1, SudokuVerifier.verify(candidateSolution));
 		
 		fail("Not yet implemented");
 	}
